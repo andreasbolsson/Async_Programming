2273023c47786e289521ad6476f5cda98d8bf389
Andreas olsson <monoxus@gmail.com>
2023-12-06 01:41:57 -0500

    Mutual exclusion.

--- Threading_App/Program.cs
+++ Threading_App/Program.cs
@@ -13,6 +13,9 @@ internal class Program
     private static Thread[] _threads;
     private static Random _rand = new Random();
     private static bool _exit = false;
+    private static int _counter = 0;
+    private static bool _useMutex = true;
+
 
     private static Mutex _mutex = new Mutex(); // Mutual exlusion synchonizer to limit work to a single thread.
 
@@ -21,12 +24,13 @@ internal class Program
         Console.WriteLine("Classic threading example with semaphore.");
 
         _exit = false;
-        _pool = new Semaphore(0,3);
+        _pool = new Semaphore(0,5);
         _threads = new Thread[5];
+        //Interlocked.Exchange(ref _useMutex, 1);
 
-        int i = 0;
+    int i = 0;
         int len = _threads.Length;
-
+        
         for(i=0; i<len; i++)
         {
             _threads[i] = new Thread(() => { DoWork(); });
@@ -34,14 +38,33 @@ internal class Program
             _threads[i].Start();
         }
 
-        _pool.Release(2);
+        _pool.Release(5);
         Console.WriteLine("Threads started.");
 
         ConsoleKeyInfo kinfo = Console.ReadKey();
+        Console.WriteLine("\r  ");
 
-        while(kinfo.Key != ConsoleKey.X)
+        while (kinfo.Key != ConsoleKey.X)
         {
+            switch(kinfo.Key)
+            {
+
+                case ConsoleKey.M:
+                    _useMutex = !_useMutex;
+                    Console.WriteLine("Switch mutual exclusion {0}.", _useMutex? "ON":"OFF" );
+
+                    break;
+                case ConsoleKey.N:
+                    //Interlocked.Exchange(ref _useMutex, 0);
+                    break;
+                default:
+                    break;
+                //
+
+            }
+
             kinfo = Console.ReadKey();
+            Console.WriteLine("\r  ");
         }
 
         Console.WriteLine("\nExiting app");
@@ -55,15 +78,28 @@ internal class Program
     private static void DoWork()
     {
         //int dur = _rand.Next(5000);
-        int dur = 1000;
+        int dur = 1000; //+ _rand.Next(10)*100;
         int n = 1;
 
         while ( !_exit )
         {
-            _pool.WaitOne();
+            _pool.WaitOne(); // Ensure only a given number of threads use resources.
 
             Thread.Sleep(dur);
-            Console.WriteLine("Thread {0} has executed {1} times", Thread.CurrentThread.Name, n);
+            //Console.WriteLine("Thread {0} has executed {1} times", Thread.CurrentThread.Name, n);
+
+            if ( _useMutex )
+            {
+                _mutex.WaitOne();
+                _counter++;
+                Console.WriteLine("{0}: _count={1};", Thread.CurrentThread.Name, _counter);
+                _mutex.ReleaseMutex();
+            }
+            else
+            {
+                Console.WriteLine("{0}: _count={1};", Thread.CurrentThread.Name, _counter);
+                _counter++;
+            }
 
             _pool.Release();
             n++;
