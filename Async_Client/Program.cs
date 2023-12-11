506a55224f032527b9b6be23783646edf21079cf
Andreas olsson <monoxus@gmail.com>
2023-12-11 00:07:48 -0500

    Fixed async on TCP service with performance counter.

--- Async_Client/Program.cs
+++ Async_Client/Program.cs
@@ -10,16 +10,19 @@ namespace Async_Client
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Enter message:");
             Console.ForegroundColor = ConsoleColor.White;
-
             string str = Console.ReadLine();
+
 
             while (str != String.Empty)
             {
+                int amount = ReadInteger("\nEnter number of times: ",1);
+                int port = ReadInteger("\nPort: ", 0);
+
                 IPAddress addr;
 
                 if (IPAddress.TryParse("127.0.0.1", out addr)) 
                 {
-                    IPEndPoint endPoint = new IPEndPoint(addr, 7089);
+                    IPEndPoint endPoint = new IPEndPoint(addr, port);
                     using (TcpClient client = new TcpClient())
                     {
                         client.Connect(endPoint);
@@ -27,13 +30,15 @@ namespace Async_Client
 
                         Byte[] b = System.Text.Encoding.UTF8.GetBytes(str);
                         
-
-                        Span<byte> bspan = new Span<byte>(b);
+                        Span<byte> bspan = new Span<byte>(b); // For speed, safely allocated a contiguous area of memoty instead of using array stored in heap.
 
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Sending data...");
 
-                        stream.Write(b);
+                        for (int i = 0; i < amount; i++) // Send message repeatedly the number of times defined by the user.
+                        {
+                            stream.Write(b); // Output the span of bytes that make up the message to the stream being sent to the local endpoint.
+                        }
 
                         Console.WriteLine("Sent.");
                         Console.ForegroundColor = ConsoleColor.White;
@@ -48,5 +53,29 @@ namespace Async_Client
                 str = Console.ReadLine();
             }
         }
+
+        private static int ReadInteger(string prompt, int min)
+        {
+            int number = 0;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(prompt);
+            var pos = Console.GetCursorPosition();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            while (!int.TryParse(Console.ReadLine(), out number) || number < min) // The number of times a message is sent must be once or more.
+            {
+                Console.SetCursorPosition(pos.Left, pos.Top);
+                Console.WriteLine("                                          ");
+                Console.SetCursorPosition(pos.Left, pos.Top);
+
+                Console.ReadLine();
+            }
+
+            return number;
+        }
+
+
     }
 }
--- Async_Service_WithPerformance/Worker.cs
+++ Async_Service_WithPerformance/Worker.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Sockets;
 
@@ -7,70 +8,167 @@ namespace Async_Service_WithPerformance
     {
         private readonly ILogger<Worker> _logger;
         private readonly IConfiguration _config;
-        private readonly int _port;
-        private TcpListener _listener;
+        private readonly int _portStart;
+        private TcpListener[] _listeners;
+        private List<Task> _tasks;
+
+        System.Diagnostics.PerformanceCounter _pcounter;
 
         public Worker(IConfiguration config, ILogger<Worker> logger)
         {
             _config = config;
             _logger = logger;
+            _listeners = new TcpListener[3];
+            _tasks = new List<Task>();
 
-            _port = config.GetValue<int>("Port"); // Get port from configuration file.
+            if( !System.Diagnostics.PerformanceCounterCategory.Exists("BytesPerNS"))
+            {
+                System.Diagnostics.CounterCreationDataCollection dataCollection = new System.Diagnostics.CounterCreationDataCollection();
+
+                System.Diagnostics.CounterCreationData counterData = new System.Diagnostics.CounterCreationData();
+                counterData.CounterName = "TestService";
+                counterData.CounterType = System.Diagnostics.PerformanceCounterType.NumberOfItems64;
+                dataCollection.Add(counterData);
+
+                System.Diagnostics.PerformanceCounterCategory.Create("BytesPerNS", "Counts number of bytes processed per nanosecond.", System.Diagnostics.PerformanceCounterCategoryType.SingleInstance, dataCollection);
+
+                while (!System.Diagnostics.PerformanceCounterCategory.Exists("BytesPerNS"))
+                {
+
+                    Task.Delay(1000); // Wait for creation of performance counter to complete
+                }
+            }
+
+            _pcounter = new System.Diagnostics.PerformanceCounter("BytesPerNS","TestService",false);
+
+            _portStart = config.GetValue<int>("Port"); // Get port from configuration file.
 
             IPAddress addr;
 
-            if (IPAddress.TryParse("127.0.0.1", out addr)) // Listen to local TCP traffic on port set in configuration file.
-            {
-                _listener = new TcpListener(addr, _port);
-            }
+            int p = _portStart;
 
+            if (IPAddress.TryParse("127.0.0.1", out addr)) // Listen to local TCP traffic starting on port set in configuration file.
+            {
+                for (int k = 0; k < _listeners.Length; k++)
+                {
+                    _listeners[k] = new TcpListener(addr, p);
+                    p++;
+                }
+            }
+        }
+
+        public override async Task StartAsync(CancellationToken stoppingToken)
+        {
+            for (int k = 0; k < _listeners.Length; k++)
+            {
+                if (_listeners[k] != null)
+                {
+                    _tasks.Add(Listen(k, stoppingToken)); // Start another listener.
+                }
+            }
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            if(_listener != null)
-            { 
-            _listener.Start();
+            //Do nothing. Listening is handled by Listen(int index, CancellationToken stoppingToken)
+        }
 
-            Console.WriteLine("Listening on port {0}.\n", _port);
+        private async Task Listen(int index, CancellationToken stoppingToken)
+        {
+            _listeners[index].Start();
 
-                while (!stoppingToken.IsCancellationRequested)
+            Console.WriteLine("Listening on port {0}.\n", ((IPEndPoint)_listeners[index].LocalEndpoint).Port);
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (_listeners[index].Pending()) // Check if there is an incoming request.
                 {
-                    if (_listener.Pending()) // Check if there is an incoming request.
+                    TcpClient client = await _listeners[index].AcceptTcpClientAsync();
+
+                    if (client != null)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("\nINCOMING MESSAGE..................");
-                        Console.WriteLine("----------------------------------");
-                        Console.ForegroundColor = ConsoleColor.White;
-
-                        using (TcpClient client = _listener.AcceptTcpClient()) // Create a client to read the incoming TCP stream/
+                        //Task t = ProcessMessage(_listeners[k]); // Start independent thread to process incoming message.
+                        try
                         {
-                            NetworkStream stream = client.GetStream();
-                            byte[] buffer = new byte[4096]; // Buffer to read chunks of data from the stream.
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nINCOMING MESSAGE ON PORT {0}..................", ((IPEndPoint)_listeners[index].LocalEndpoint).Port);
+                            Console.ForegroundColor = ConsoleColor.White;
 
-                            int n = stream.Read(buffer, 0, buffer.Length);
+                            ReceiveMessage(client);
 
-                            while (n > 0)
-                            {
-                                for (int i = 0; i < n; i++)
-                                {
-                                    if (buffer[i] > 0)
-                                    {
-                                        Char c = Convert.ToChar(buffer[i]);
-                                        Console.Write(c);
-                                    }
-                                }
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("\n--END OF MESSAGE--");
+                            Console.ForegroundColor = ConsoleColor.White;
 
-                                n = stream.Read(buffer, 0, buffer.Length);
-                            }
                         }
+                        catch (Exception exc)
+                        {
+                            _logger.Log(LogLevel.Error, exc.Message,new object[0]); // Record exception.
+                        }
+                        finally
+                        {
+                            client.Close(); //Make sure connection is closed so listener continues listen to tcp traffic.
 
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("\n---------END OF MESSAGE-----------");
-                        Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        client.Close();
                     }
+                }
 
-                    await Task.Delay(100, stoppingToken);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                await Task.Delay(100, stoppingToken);
+            }
+        }
+        private void ReceiveMessage( TcpClient client )
+        {
+            long x = 0;
+
+            DateTime timeZero = DateTime.UtcNow;
+            DateTime time = DateTime.UtcNow;
+            TimeSpan dur = TimeSpan.Zero;
+
+            long count = 0;
+
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[4096]; // Buffer to read chunks of data from the stream.
+
+            int n = stream.Read(buffer, 0, buffer.Length);
+            count = n;
+
+            while (n > 0)
+            {
+                time = DateTime.UtcNow;
+
+                OutputToConsole(buffer, n); //Message won't necessarily be in order, but make asunc so performance monitor measure speed of receiving bytes.
+
+                dur = DateTime.UtcNow - time;
+                _pcounter.RawValue = dur.Ticks * 100 / n;
+
+                n = stream.Read(buffer, 0, buffer.Length);
+                count += n;
+            }
+
+            
+            dur = DateTime.UtcNow - timeZero;
+
+
+            if (count > 0) // Prevent division by zero error just in case (should not happen though).
+            {
+                x = dur.Ticks * 100 / count;
+            }
+
+
+            Console.WriteLine("\nbytes/ns: {0}", x);
+        }
+
+        static async Task OutputToConsole( byte[] b, int r)
+        {
+            for (int i = 0; i < r; i++)
+            {
+                if (b[i] > 0)
+                {
+                    Char c = Convert.ToChar(b[i]);
+                    Console.Write(c);
                 }
             }
         }
